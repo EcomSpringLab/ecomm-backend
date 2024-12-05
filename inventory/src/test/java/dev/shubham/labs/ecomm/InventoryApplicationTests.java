1c3b776c2987b74d911b078a8c0a5fe90ae000b9
shubham <shubhamgoyal23@yahoo.com>
2024-12-05 18:55:43 +0530

    adding inventory module

---
+++ inventory/src/test/java/dev/shubham/labs/ecomm/InventoryApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.shubham.labs.ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class InventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
