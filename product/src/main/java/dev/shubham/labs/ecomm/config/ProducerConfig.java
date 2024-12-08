55995f0b5ed25d187e88ec9b2ab55af6225af58b
shubham <shubhamgoyal23@yahoo.com>
2024-12-09 00:05:25 +0530

    updating product module

--- product/src/main/java/dev/shubham/labs/ecomm/config/ProducerConfig.java
+++ product/src/main/java/dev/shubham/labs/ecomm/config/ProducerConfig.java
@@ -6,6 +6,7 @@ import dev.shubham.labs.ecomm.kafka.producer.KafkaProducerService;
 import dev.shubham.labs.ecomm.kafka.producer.KafkaProducerServiceImpl;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.observation.ObservationRegistry;
+import io.opentelemetry.api.trace.Tracer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -27,9 +28,9 @@ public class ProducerConfig {
     @Bean
     public KafkaProducerService<String, AllocateInventoryEvent> allocateInventoryProducerService(
             KafkaProducerConfig kafkaProducerConfig, MeterRegistry meterRegistry, ObservationRegistry registry,
-            ExecutorService kafkaProducerExecutor) {
+            ExecutorService kafkaProducerExecutor, Tracer tracer) {
         return new KafkaProducerServiceImpl<>(kafkaProducerConfig.getInstances().get("inventory"), StringSerializer.class, JsonSerializer.class,
-                meterRegistry, registry, kafkaProducerExecutor) {
+                meterRegistry, registry, kafkaProducerExecutor, tracer) {
         };
     }
 
